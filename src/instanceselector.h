1911f3cdc32bb2b30671670efc0ea398c8a62589
Montel Laurent <montel@kde.org>
2017-01-17 14:05:47 +0100

    Create a lib to allow to create autotest in the future

--- src/instanceselector.h
+++ src/instanceselector.h
@@ -23,6 +23,7 @@
 #define INSTANCESELECTOR_H
 
 #include "mainwindow.h"
+#include "akonadiconsolelib_export.h"
 
 #include <QDialog>
 #include <KConfigGroup>
@@ -35,7 +36,7 @@ class InstanceSelector;
 /** Check if there are multiple instances of Akonadi running, and if so present
  *  a list to select the one to connect to.
  */
-class InstanceSelector : public QDialog
+class AKONADICONSOLELIB_EXPORT InstanceSelector : public QDialog
 {
     Q_OBJECT
 public:
