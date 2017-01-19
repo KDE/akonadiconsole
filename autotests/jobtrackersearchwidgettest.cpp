ec46839b5fabe198447f7598afff1893ceadda71
Montel Laurent <montel@kde.org>
2017-01-19 07:16:04 +0100

    Add searchlineedit component

---
+++ autotests/jobtrackersearchwidgettest.cpp
@@ -0,0 +1,64 @@
+/*
+  Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "jobtrackersearchwidgettest.h"
+#include "jobtrackersearchwidget.h"
+#include <QTest>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QSignalSpy>
+
+JobTrackerSearchWidgetTest::JobTrackerSearchWidgetTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+JobTrackerSearchWidgetTest::~JobTrackerSearchWidgetTest()
+{
+
+}
+
+void JobTrackerSearchWidgetTest::shouldHaveDefaultValue()
+{
+    JobTrackerSearchWidget w;
+    QHBoxLayout * mainLayout = w.findChild<QHBoxLayout *>(QStringLiteral("mainlayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->margin(), 0);
+
+
+    QLineEdit *mSearchLineEdit = w.findChild<QLineEdit *>(QStringLiteral("searchline"));
+    QVERIFY(mSearchLineEdit);
+    QVERIFY(mSearchLineEdit->isClearButtonEnabled());
+    QVERIFY(mSearchLineEdit->text().isEmpty());
+}
+
+void JobTrackerSearchWidgetTest::shouldEmitSignal()
+{
+    JobTrackerSearchWidget w;
+    QSignalSpy searchWidgetSignal(&w, SIGNAL(searchTextChanged(QString)));
+    QLineEdit *mSearchLineEdit = w.findChild<QLineEdit *>(QStringLiteral("searchline"));
+    const QString foo = QStringLiteral("foo");
+    mSearchLineEdit->setText(foo);
+    QCOMPARE(searchWidgetSignal.count(), 1);
+    QCOMPARE(searchWidgetSignal.at(0).at(0).toString(), foo);
+    mSearchLineEdit->clear();
+    QCOMPARE(searchWidgetSignal.count(), 2);
+    QCOMPARE(searchWidgetSignal.at(1).at(0).toString(), QString());
+}
+
+QTEST_MAIN(JobTrackerSearchWidgetTest)
---
+++ src/jobtrackersearchwidget.cpp
@@ -0,0 +1,41 @@
+/*
+  Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "jobtrackersearchwidget.h"
+
+#include <QHBoxLayout>
+#include <QLineEdit>
+
+JobTrackerSearchWidget::JobTrackerSearchWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QHBoxLayout *mainLayout = new QHBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainlayout"));
+    mainLayout->setMargin(0);
+
+    mSearchLineEdit = new QLineEdit(this);
+    mSearchLineEdit->setObjectName(QStringLiteral("searchline"));
+    mSearchLineEdit->setClearButtonEnabled(true);
+    mSearchLineEdit->setPlaceholderText(QStringLiteral("Search..."));
+    mainLayout->addWidget(mSearchLineEdit);
+    connect(mSearchLineEdit, &QLineEdit::textChanged, this, &JobTrackerSearchWidget::searchTextChanged);
+}
+
+JobTrackerSearchWidget::~JobTrackerSearchWidget()
+{
+
+}
