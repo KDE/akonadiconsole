120a96476b6d4cc85fdfb51e19fa3c5327c4125d
Laurent Montel <montel@kde.org>
2021-08-30 07:48:27 +0200

    Use qOverload directly (scripted)

--- src/searchwidget.cpp
+++ src/searchwidget.cpp
@@ -101,7 +101,7 @@ SearchWidget::SearchWidget(QWidget *parent)
 
     connect(button, &QPushButton::clicked, this, &SearchWidget::search);
     connect(mDatabaseView, &QListView::activated, this, &SearchWidget::fetchItem);
-    connect(mStoreCombo, QOverload<int>::of(&KComboBox::currentIndexChanged), this, &SearchWidget::openStore);
+    connect(mStoreCombo, qOverload<int>(&KComboBox::currentIndexChanged), this, &SearchWidget::openStore);
 
     openStore(0);
 
