f829eb2fdfa69d6f0606698a0d3d6d50c679d307
Laurent Montel <montel@kde.org>
2021-10-15 08:19:55 +0200

    GIT_SILENT: Remove old kconf_update

--- src/kconf_update/akonadiconsole-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/akonadiconsole.desktop/\/org.kde.akonadiconsole.desktop/" $kickoffrcname
-fi
