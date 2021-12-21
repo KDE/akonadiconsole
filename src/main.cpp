03e01a0f5a93efa03d67220b7bc0dc7156139f9b
Laurent Montel <montel@kde.org>
2021-12-21 18:46:47 +0100

    GIT_SILENT: AA_EnableHighDpiScaling is by default in qt6 now

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,9 @@
 
 int main(int argc, char **argv)
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+#endif
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
     QApplication app(argc, argv);
 #if KCOREADDONS_VERSION < QT_VERSION_CHECK(6, 0, 0)
