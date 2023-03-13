59cad7a59039b2d2b3b0caa1f5ed9d420bb6461b
Laurent Montel <montel@kde.org>
2023-03-13 13:31:01 +0100

    GIT_SILENT: fix copyright

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
                          QStringLiteral(KDEPIM_VERSION),
                          i18n("The Management and Debugging Console for Akonadi"),
                          KAboutLicense::GPL,
-                         i18n("(c) 2006-2022 the Akonadi developer"),
+                         i18n("(c) 2006-2023 the Akonadi developer"),
                          QString(),
                          QStringLiteral("https://community.kde.org/KDE_PIM/akonadi"));
     QApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("akonadi")));
